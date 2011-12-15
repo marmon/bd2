ce1ab3d50914eb31aa448e978a50fe16710974f9
Marek Lewandowski <marmonn@gmail.com>
2011-12-15 18:46:40 +0100

    commit testowy

--- BD2/src/view/MainWindow.java
+++ BD2/src/view/MainWindow.java
@@ -38,6 +38,7 @@ public class MainWindow extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		// maro to byl
 	}
 
 }

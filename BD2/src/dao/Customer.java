1a74ff6812a8ae5a5ec5a7b14f897cf760c2843f
Marek Lewandowski <marmonn@gmail.com>
2011-12-16 13:51:13 +0100

    Added fields to Customer entity class

--- BD2/src/dao/Customer.java
+++ BD2/src/dao/Customer.java
@@ -2,5 +2,85 @@ package dao;
 
 // entity class
 public class Customer {
-
+	private int id;
+	private String documentID;
+	private String name;
+	private String lastName;
+	private int phoneNumber;
+	private String country;
+	private String city;
+	private String postCode;
+	private String street;
+	private String streetNumber;
+	private int flatNumber;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDocumentID() {
+		return documentID;
+	}
+	public void setDocumentID(String documentID) {
+		this.documentID = documentID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getStreetNumber() {
+		return streetNumber;
+	}
+	public void setStreetNumber(String streetNumber) {
+		this.streetNumber = streetNumber;
+	}
+	public int getFlatNumber() {
+		return flatNumber;
+	}
+	public void setFlatNumber(int flatNumber) {
+		this.flatNumber = flatNumber;
+	}
+	
+	
 }
